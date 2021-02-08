8c1be9909caaf06b2fd5a98d47274e2715775549
ALEXANDER BEERS <arbeers@wisc.edu>
2021-02-08 15:47:32 -0600

    Made animated menu on scroll

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-var yPos = 0;
 var animationInProgress = false;
+var menuDown = false;
+var yPos = 0;
+var element;
+var currPos;
+var height;
+
 
 window.onload = function () {
+    element = document.getElementById("onScrollMenu");
+    currPos = element.offsetTop;
+    height = element.offsetTop;
     window.onscroll = function () {
         var newY = window.pageYOffset;
-        if (newY < yPos) {
-            animate();
+        if (newY < yPos && !animationInProgress) {
+            animateVisible();
+        } else if (newY > yPos && menuDown) {
+            animateHidden();
         }
+        animationInProgress = false;
         yPos = newY;
     }
 }
 
-function animate() {
-    console.log("animate");
+function animateVisible() {
+    animationInProgress = true;
+    var timer = setInterval(function () {
+        if (currPos < 0) {
+            currPos++;
+            element.style.top = currPos + 'px';
+            console.log(currPos);
+        } else {
+            clearInterval(timer);
+        }
+    }, 20);
+    menuDown = true;
 }
+
+function animateHidden() {
+    var timer = setInterval(function () {
+        if (currPos >= height) {
+            currPos--;
+            element.style.top = currPos + 'px';
+            console.log(currPos);
+        } else {
+            clearInterval(timer);
+        }
+    }, 7);
+    menuDown = false;
+}
+
